44fec3193b09575df3e655f1db5a784baddf84f9
Speckles zzzz <speckleszzzz@gmail.com>
2024-03-05 05:35:59 +0000

    welcome page layout over

---
+++ src/Components/TrendingCreators.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+const TrendingCreators = () => {
+  return (
+    <div>
+      <div>
+        <p>Trending rn</p>
+      </div>
+      <div>
+        
+      </div>
+    </div>
+  );
+}
+
+export default TrendingCreators;
--- src/Components/Welcome.js
+++ src/Components/Welcome.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import welcomewallpaper from "../images/welcomewallpaper.jpg";
 
-
 const Welcome = () => {
   return (
-    <div className="bg-[url("../images/welcomewallpaper.jpg")]">
-        <div className='text-center'>
+    <div style={{ backgroundImage: `url(${welcomewallpaper})` }} className="bg-cover bg-center h-screen">
+        <div className='text-center text-white'>
             <p className='text-3xl'>Welcome to</p>
             <p className='text-5xl'>Vtuber Hub</p>
             <p>Where Aspiring Vtubers Meet Creators - Your Gateway to Virtual Stardom!</p>
@@ -14,4 +13,4 @@ const Welcome = () => {
   );
 }
 
-export default Welcome; 
+export default Welcome;
