958567c2c81f9076a4a284a9d8fa257f8beeb37b
Speckles zzzz <speckleszzzz@gmail.com>
2024-03-06 07:13:19 +0000

    trending page slider update

--- src/Components/TrendingCreators.js
+++ src/Components/TrendingCreators.js
@@ -1,17 +1,16 @@
 import React from 'react';
-import  data  from '../imagecontent';
+import data from '../imagecontent';
 import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
 
-
 const SlideLeft = () => {
-  var slider = document.getElementById('slider')
-  slider.scrollLeft = slider.scrollLeft-500
-}
+  var slider = document.getElementById('slider');
+  slider.scrollLeft = slider.scrollLeft - 500;
+};
 
 const SlideRight = () => {
-  var slider = document.getElementById('slider')
-  slider.scrollLeft = slider.scrollLeft+500
-}
+  var slider = document.getElementById('slider');
+  slider.scrollLeft = slider.scrollLeft + 500;
+};
 
 const TrendingCreators = () => {
   return (
@@ -24,12 +23,20 @@ const TrendingCreators = () => {
         <MdChevronLeft className='opacity-50 cursor-pointer hover:opacity-100' size={50} onClick={SlideLeft} />
         <div id="slider" className='w-full h-full overflow-x-scroll scroll whitespace-nowrap scroll-smooth space-x-4 scrollbar-hide'>
           {data.map((items, index) => (
-            <div key={index} className="inline-block">
-              <img className='w-[320px] inline-block p-2 cursor-pointer hover:scale-105 ease-in-out duration-300' src={items.img} alt='/' />
-              <p>{items.name}</p>
-              <p>{items.description}</p>
-              <p>{items.genre}</p>
+            <div key={index} className="inline-block relative">\
+            <div className='w-[300px] h-[300px]'>
+              <div > 
+                <img className='object-fill inline-block p-2 cursor-pointer hover:scale-105 ease-in-out duration-300' src={items.img} alt='/' />
+              </div>
+              
+                <div className='bg-black bg-opacity-50 text-white p-4 '>
+                  <p>{items.name}</p>
+                  <p className="text-balance">{items.description}</p>
+                  <p>{items.genre}</p>
+                </div>
+              </div>
             </div>
+              
           ))}
         </div>
         <MdChevronRight className='opacity-50 cursor-pointer hover:opacity-100' size={50} onClick={SlideRight} />
