005c62bfa482ef97c829b28733d81e9a79b504dd
Speckles zzzz <speckleszzzz@gmail.com>
2024-04-03 09:21:45 +0000

    serve

--- src/api/server.js
+++ src/api/server.js
@@ -3,11 +3,15 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 
 const app = express();
-
-app.use(cors());
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 app.use(express.json());
 
-const CONNECTION_STRING = 'mongodb+srv://akashcharles:englandss2406@cluster0.lxuq4oq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+const CONNECTION_STRING = 'mongodb+srv://akashcharles:englandss2406@cluster0.lxuq4oq.mongodb.net/PostItems?retryWrites=true&w=majority&appName=Cluster0';
 mongoose.connect(CONNECTION_STRING, {
     useNewUrlParser: true,
     useUnifiedTopology: true
